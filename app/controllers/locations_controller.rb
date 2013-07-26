4295dbd1d71e081791423f68190a46c51c441366
Bryan <bkapicka@gmail.com>
2013-07-26 19:11:33 -0400

    updated location show action

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,11 +1,10 @@
 class LocationsController < ApplicationController
-  before_action :set_location, only: [:show, :edit, :update, :destroy]
 
   def index
     @locations = Location.all
 
     if params[:v] == "new" # GET
-      
+  
       @location = Location.new
       @path = 'locations/?v=create'
 
@@ -26,47 +25,41 @@ class LocationsController < ApplicationController
     #else # GET
       # Additional code for the scaffolded index action would go here
     end
-    
   end
+
 
   def show
-  end
+    @location = Location.find(params[:id])
 
+    if params[:v] == "edit" # GET
 
-  # GET /locations/1/edit
-  def edit
-  end
+      @path = '/notes/'+params[:id]+'?v=update'
 
-  # PATCH/PUT /locations/1
-  # PATCH/PUT /locations/1.json
-  def update
-    respond_to do |format|
-      if @location.update(location_params)
-        format.html { redirect_to @location, notice: 'Location was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @location.errors, status: :unprocessable_entity }
+    elsif params[:v] == "update" # PATCH/PUT
+
+      respond_to do |format|
+        if @location.update(location_params)
+          format.html { redirect_to @location, notice: 'Location was successfully updated.' }
+          format.json { head :no_content }
+        else
+          format.html { render action: 'edit' }
+          format.json { render json: @location.errors, status: :unprocessable_entity }
+        end
       end
+
+    elsif params[:v] == "destroy" # DELETE
+
+      @location.destroy
+      respond_to do |format|
+        format.html { redirect_to locations_url }
+        format.json { head :no_content }
+      end
+
     end
   end
 
-  # DELETE /locations/1
-  # DELETE /locations/1.json
-  def destroy
-    @location.destroy
-    respond_to do |format|
-      format.html { redirect_to locations_url }
-      format.json { head :no_content }
-    end
-  end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_location
-      @location = Location.find(params[:id])
-    end
-
     # Never trust parameters from the scary internet, only allow the white list through.
     def location_params
       params.require(:location).permit(:name)
