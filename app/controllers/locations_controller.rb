a36c9ad5999848b854c844819fb5b37974985743
Bryan <bkapicka@gmail.com>
2013-07-26 19:07:18 -0400

    updated location index action

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,40 +1,40 @@
 class LocationsController < ApplicationController
   before_action :set_location, only: [:show, :edit, :update, :destroy]
 
-  # GET /locations
-  # GET /locations.json
   def index
     @locations = Location.all
+
+    if params[:v] == "new" # GET
+      
+      @location = Location.new
+      @path = 'locations/?v=create'
+
+    elsif params[:v] == "create" # POST
+
+      @location = Location.new(location_params)
+
+      respond_to do |format|
+        if @location.save
+          format.html { redirect_to @location, notice: 'Location was successfully created.' }
+          format.json { render action: 'show', status: :created, location: @location }
+        else
+          format.html { render action: 'new' }
+          format.json { render json: @location.errors, status: :unprocessable_entity }
+        end
+      end
+
+    #else # GET
+      # Additional code for the scaffolded index action would go here
+    end
+    
   end
 
-  # GET /locations/1
-  # GET /locations/1.json
   def show
   end
 
-  # GET /locations/new
-  def new
-    @location = Location.new
-  end
 
   # GET /locations/1/edit
   def edit
-  end
-
-  # POST /locations
-  # POST /locations.json
-  def create
-    @location = Location.new(location_params)
-
-    respond_to do |format|
-      if @location.save
-        format.html { redirect_to @location, notice: 'Location was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @location }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @location.errors, status: :unprocessable_entity }
-      end
-    end
   end
 
   # PATCH/PUT /locations/1
